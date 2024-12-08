3b528cc404c6301c585440e361d9fcb46b760856
Elinn-16 <martinaenievas@escuelasproa.edu.ar>
2024-12-08 17:18:50 -0300

    act04 no me sale

---
+++ act04.js
@@ -0,0 +1,14 @@
+function reverseString(str) {
+    if (str === "") {
+        return "Candena vacia";
+    }
+    if (str === 1) {
+        return str;
+    }
+    if (str > 1) {
+        return (reverseString(str.slice(1) + str[0]));
+    }
+}
+
+console.log(reverseString(""));
+
